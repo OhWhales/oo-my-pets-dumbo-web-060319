0382705fa8433eb3a4738d20a9ce7b56c2c53a1c
impartial-flow-3347 <brandon.lau86@gmail.com>
2019-06-11 15:30:16 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,55 @@
 class Owner
   # code goes here
+
+  attr_accessor :name, :pets
+  attr_reader :species 
+  def initialize(species)
+    @species = species 
+    @pets = 
+    {
+      :fishes => [],
+      :dogs => [],
+      :cats => []
+    }
+  end
+    
+  def say_species 
+    return "I am a #{@species}."
+  end 
+  
+  def buy_fish(fish)
+    @pets[:fishes].push(Fish.new(fish))
+  end
+  def buy_cat(cat) 
+    @pets[:cats].push(Cat.new(cat))
+  end
+  def buy_dog(dog)
+    @pets[:dogs].push(Dog.new(dog))
+  end
+  def walk_dogs 
+    @pets[:dogs].each { |dog| dog.mood = "happy" }
+  end
+  def play_with_cats 
+    @pets[:cats].each { |cat| cat.mood = "happy" }
+  end
+  def feed_fish
+    @pets[:fishes].each do |fish|
+      fish.mood = "happy"
+    end
+  end 
+  
+  def sell_pets
+    @pets.each do |k,v|
+      v.each do |animal|
+        animal.mood = "nervous"
+      end
+      v.clear 
+    end
+  end
+  
+  def list_pets
+    
+  end
+  
+  
 end
